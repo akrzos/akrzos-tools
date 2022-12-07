d64bc2b4c21b94b829fc90ad272e40736e00acd1
Alex Krzos <akrzos@redhat.com>
2022-12-07 13:42:40 -0500

    Add patching the placementrules container to ACM 2.7 patches

--- rhacm/acm-2.7-patches.sh
+++ rhacm/acm-2.7-patches.sh
@@ -7,10 +7,23 @@ oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json
 oc annotate multiclusterengine multiclusterengine pause=true
 oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json |  jq '.spec.template.spec.containers[0].resources.limits.memory = "16Gi"' | oc replace -f -
 oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
-echo "Done Patching"
+echo "Sleep 45"
+sleep 45
 
 echo "Patching MCE ocm-webhook memory limits to 4Gi"
 oc get deploy -n multicluster-engine ocm-webhook -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
 oc get deploy -n multicluster-engine ocm-webhook -o json |  jq '.spec.template.spec.containers[0].resources.limits.memory = "4Gi"' | oc replace -f -
 oc get deploy -n multicluster-engine ocm-webhook -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
+echo "Sleep 45"
+sleep 45
+
+echo "Patching ACM multicluster-operators-application multicluster-operators-placementrule memory limits to 2Gi"
+oc get csv -n open-cluster-management advanced-cluster-management.v2.7.0 -o json |  jq '.spec.install.spec.deployments[] | select(.name=="multicluster-operators-application").spec.template.spec.containers[] | select(.name=="multicluster-operators-placementrule").resources.limits.memory'
+oc get deploy -n open-cluster-management multicluster-operators-application -o json |  jq '.spec.template.spec.containers[] | select(.name=="multicluster-operators-placementrule").resources.limits.memory'
+oc get csv -n open-cluster-management advanced-cluster-management.v2.7.0 -o json |  jq '.spec.install.spec.deployments[] |= (select(.name=="multicluster-operators-application").spec.template.spec.containers[] |= (select(.name=="multicluster-operators-placementrule").resources.limits.memory = "2Gi"))' | oc replace -f -
+oc get csv -n open-cluster-management advanced-cluster-management.v2.7.0 -o json |  jq '.spec.install.spec.deployments[] | select(.name=="multicluster-operators-application").spec.template.spec.containers[] | select(.name=="multicluster-operators-placementrule").resources.limits.memory'
+oc get deploy -n open-cluster-management multicluster-operators-application -o json |  jq '.spec.template.spec.containers[] | select(.name=="multicluster-operators-placementrule").resources.limits.memory'
+echo "Sleep 45"
+sleep 45
+
 echo "Done Patching"
