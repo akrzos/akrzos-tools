50206ccb378ea246fd159ced75c02c1ecdbf173f
Alex Krzos <akrzos@redhat.com>
2023-01-06 10:24:17 -0500

    All current memory patches are fixed in ACM 2.7

--- rhacm/acm-2.7-patches.sh
+++ rhacm/acm-2.7-patches.sh
@@ -2,28 +2,31 @@
 
 export KUBECONFIG=/root/bm/kubeconfig
 
-echo "Patching MCE managedcluster-import-controller-v2 memory limits to 16Gi"
-oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
-oc annotate multiclusterengine multiclusterengine pause=true
-oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json |  jq '.spec.template.spec.containers[0].resources.limits.memory = "16Gi"' | oc replace -f -
-oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
-echo "Sleep 45"
-sleep 45
+# Fixed - https://issues.redhat.com/browse/ACM-2275
+# echo "Patching MCE managedcluster-import-controller-v2 memory limits to 16Gi"
+# oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
+# oc annotate multiclusterengine multiclusterengine pause=true
+# oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json |  jq '.spec.template.spec.containers[0].resources.limits.memory = "16Gi"' | oc replace -f -
+# oc get deploy -n multicluster-engine managedcluster-import-controller-v2 -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
+# echo "Sleep 45"
+# sleep 45
 
-echo "Patching MCE ocm-webhook memory limits to 4Gi"
-oc get deploy -n multicluster-engine ocm-webhook -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
-oc get deploy -n multicluster-engine ocm-webhook -o json |  jq '.spec.template.spec.containers[0].resources.limits.memory = "4Gi"' | oc replace -f -
-oc get deploy -n multicluster-engine ocm-webhook -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
-echo "Sleep 45"
-sleep 45
+# Fixed - https://issues.redhat.com/browse/ACM-2305
+# echo "Patching MCE ocm-webhook memory limits to 4Gi"
+# oc get deploy -n multicluster-engine ocm-webhook -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
+# oc get deploy -n multicluster-engine ocm-webhook -o json |  jq '.spec.template.spec.containers[0].resources.limits.memory = "4Gi"' | oc replace -f -
+# oc get deploy -n multicluster-engine ocm-webhook -o json | jq '.spec.template.spec.containers[0].resources.limits.memory'
+# echo "Sleep 45"
+# sleep 45
 
-echo "Patching ACM multicluster-operators-application multicluster-operators-placementrule memory limits to 4Gi"
-oc annotate mch -n open-cluster-management multiclusterhub mch-pause=True
-oc get deploy -n open-cluster-management multicluster-operators-application -o json |  jq '.spec.template.spec.containers[] | select(.name=="multicluster-operators-placementrule").resources.limits.memory'
-oc get deploy -n open-cluster-management multicluster-operators-application -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="multicluster-operators-placementrule").resources.limits.memory = "4Gi")' | oc replace -f -
-oc get deploy -n open-cluster-management multicluster-operators-application -o json |  jq '.spec.template.spec.containers[] | select(.name=="multicluster-operators-placementrule").resources.limits.memory'
-echo "Sleep 45"
-sleep 45
+# Fixed - https://issues.redhat.com/browse/ACM-2336
+# echo "Patching ACM multicluster-operators-application multicluster-operators-placementrule memory limits to 4Gi"
+# oc annotate mch -n open-cluster-management multiclusterhub mch-pause=True
+# oc get deploy -n open-cluster-management multicluster-operators-application -o json |  jq '.spec.template.spec.containers[] | select(.name=="multicluster-operators-placementrule").resources.limits.memory'
+# oc get deploy -n open-cluster-management multicluster-operators-application -o json |  jq '.spec.template.spec.containers[] |= (select(.name=="multicluster-operators-placementrule").resources.limits.memory = "4Gi")' | oc replace -f -
+# oc get deploy -n open-cluster-management multicluster-operators-application -o json |  jq '.spec.template.spec.containers[] | select(.name=="multicluster-operators-placementrule").resources.limits.memory'
+# echo "Sleep 45"
+# sleep 45
 
 # Method when placement rules container is in the CSV
 # echo "Patching ACM multicluster-operators-application multicluster-operators-placementrule memory limits to 2Gi"
