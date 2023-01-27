273d5eac74eef9405cd04b146897688ee9967567
Alex Krzos <akrzos@redhat.com>
2023-01-27 14:28:25 -0500

    Fix acm 2.7 patch script, (Missing closing brackets)

--- rhacm/acm-2.7-patches.sh
+++ rhacm/acm-2.7-patches.sh
@@ -20,17 +20,17 @@ export KUBECONFIG=/root/bm/kubeconfig
 # sleep 45
 
 echo "Applying ACM search-v2-operator collector resources bump"
-oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/collector/resources", "value": {"limits": {"memory": "16Gi"}}]'
+oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/collector/resources", "value": {"limits": {"memory": "16Gi"}}}]'
 # oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/collector/resources", "value": {"limits": {"memory": "16Gi", "cpu": "2"}, "requests": {"memory": "32Mi", "cpu": "25m"}}}]'
 echo "Sleep 10"
 sleep 10
 echo "Applying ACM search-v2-operator database resources bump"
-oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/database/resources", "value": {"limits": {"memory": "16Gi"}}]'
+oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/database/resources", "value": {"limits": {"memory": "16Gi"}}}]'
 # oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/database/resources", "value": {"limits": {"memory": "16Gi", "cpu": "2"}, "requests": {"memory": "32Mi", "cpu": "25m"}}}]'
 echo "Sleep 10"
 sleep 10
 echo "Applying ACM search-v2-operator indexer resources bump"
-oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/indexer/resources", "value": {"limits": {"memory": "16Gi"}}]'
+oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/indexer/resources", "value": {"limits": {"memory": "16Gi"}}}]'
 # oc patch search -n open-cluster-management search-v2-operator --type json -p '[{"op": "add", "path": "/spec/deployments/indexer/resources", "value": {"limits": {"memory": "16Gi", "cpu": "2"}, "requests": {"memory": "32Mi", "cpu": "25m"}}}]'
 echo "Sleep 10"
 sleep 10
